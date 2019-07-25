bf4befbbf7ad97a715862c451c2c36e654c7c078
Soren Hopkins <soren.hopkins@gmail.com>
2019-07-25 14:37:45 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -36,13 +36,24 @@ function checkForMatch() {
 		}
 	}
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
 	console.log("User flipped " + cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].suit);
 	console.log("User flipped " + cards[cardId].cardImage);
+	this.setAttribute("src", cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 	}
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+		}
+	}
+
+createBoard();
